dc54f2867872cdab28337ecf3c567d8947f05a57
Mochi-coder <116069253+Mochi-coder@users.noreply.github.com>
2023-01-13 20:09:01 +0000

    first questions started

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,3 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('choice-text'));
+const progressText = document.querySelector('#score');
---
+++ starter/assets/js/questions.js
@@ -0,0 +1,12 @@
+var quizQuestions = [
+    {
+        question: "What does the JSON stand for?",
+        answers: [
+        "JavaScript Object Notation",
+        "Just Some Object Nonsense",
+        "JavaScript On Network",
+        "Just SomeOne's Name"
+],
+correctAnswerIndex: 0
+},
+]
