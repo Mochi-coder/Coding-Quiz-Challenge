051d3e747614f980e51dd6e359babb587e9a0938
Mochi-coder <116069253+Mochi-coder@users.noreply.github.com>
2023-01-17 10:26:00 +0000

    questions complete

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -9,4 +9,44 @@ var quizQuestions = [
 ],
 correctAnswerIndex: 0
 },
+{
+    question: "Which are examples of JavaScript data types?",
+    answers: [
+    "Heading, Title, Body",
+    "Numbers, Letters, Symbols",
+    "Boolean, String, Object",
+    "Circle, Square, Octagon"
+],
+correctAnswerIndex: 2
+},
+{
+    question: "Which is a type of loop struction?",
+    answers: [
+    "Wait",
+    "For",
+    "It",
+    "Here"
+],
+correctAnswerIndex: 1
+},
+{
+    question: "What are the type of pop-up boxes available in JavaScript?",
+    answers: [
+    "Alert",
+    "Confirm",
+    "Prompt",
+    "All of the above"
+],
+correctAnswerIndex: 3
+},
+{
+    question: "What is Event Bubbling?",
+    answers: [
+    "Javascript allows DOM elements to be nested in each other",
+    "A list of elements",
+    "A party",
+    "A webpage"
+],
+correctAnswerIndex: 0
+},
 ]
